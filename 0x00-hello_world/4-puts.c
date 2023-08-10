9cf90ccb0bdad0c774bad870ba39ef12a800926e
Favour-234 <agboolafavour342@gmail.com>
2023-08-10 09:07:00 -0700

    using the puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - a program that prints with the puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
