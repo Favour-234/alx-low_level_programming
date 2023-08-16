28e1128657b2b373286acb2eb60b96fb6407b162
Favour-234 <agboolafavour342@gmail.com>
2023-08-16 01:16:39 -0700

    lowaercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make lowercase alphabeth
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
