f561df2254b42e272bb5b16ecdff65e2406e7821
Favour-234 <agboolafavour342@gmail.com>
2023-08-16 12:54:18 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - print to add number
+ * @n1: The first number to be computed
+ * @n2: the second number to be computed
+ * Return: integar value
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
