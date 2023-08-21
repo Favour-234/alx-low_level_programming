004dbca52cda866f0779612c9832ba075478ee3e
Favour-234 <agboolafavour342@gmail.com>
2023-08-21 10:17:49 -0700

    5-rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse this string
+ * @s: the string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int length = 0, index = 0;
+	char tmp;
+
+	while (s[index++])
+	length++;
+
+	for (index = length - 1; index >= length / 2; index--)
+	{
+		tmp = s[index];
+		s[index] = s[length - index - 1];
+		s[length - index - 1] = tmp;
+	}
+}
