2fa822c4c1842a85ee1db7b23be8a79f8a916a65
Favour-234 <agboolafavour342@gmail.com>
2023-08-24 04:27:53 -0700

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - a function encoding rot13
+ * @s: pointer to string parameters
+ *
+ * Return: return this str
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	for (j = 0; j < 52; j++)
+	{
+		if (s[i] == data1[j])
+		{
+		s[i] = datarot[j];
+			break;
+		}
+	}
+	}
+	return (s);
+}
