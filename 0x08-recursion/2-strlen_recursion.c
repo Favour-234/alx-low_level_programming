565f3721fab5a0b7a837cf831c3245f221f77dee
Favour-234 <agboolafavour342@gmail.com>
2023-08-31 02:53:35 -0700

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - print the length of a string
+ * @s: the input.
+ *
+ * Return: The length of a string.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return ((1) + _strlen_recursion(s + 1));
+}
