a205c8da10a61de0bebbc1f972655060ee8d3ac1
Favour-234 <agboolafavour342@gmail.com>
2023-09-04 11:46:38 -0700

    a program that add positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,70 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: Always 0 (Success)
+ */
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str)) /*count string*/
+
+	{
+		if (!isdigit(str[count])) /*check if string there are digit*/
+		{
+			return (0);
+		}
+
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - Print the name of the program
+ * @argc: To Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+
+{
+
+	/*Declaring variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc) /*Goes through the whole array*/
+	{
+	if (check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]); /*ATOI --> convert the string to int*/
+		sum += str_to_int;
+	}
+
+		/*Condition for - if one of the number contains symbols that are not digits*/
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		count++;
+	}
+
+	printf("%d\n", sum); /*print sum*/
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,6 +3,7 @@
 
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[]);
+int check_num(char *str);
 int _putchar(char c);
 int _atoi(char *s);
 
