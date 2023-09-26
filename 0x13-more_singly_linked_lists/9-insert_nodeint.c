33e59a54b2ff134f7040adf6f86240ffbb54ac1e
Favour-234 <agboolafavour342@gmail.com>
2023-09-26 04:40:35 -0700

    insert list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,48 +2,45 @@
 #include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a listint_t list.
- * @head: Pointer to a pointer to the head of the list.
- * @idx: Index at which the new node should be added (starting from 0).
- * @n: The value to be stored in the new node.
+ * insert_nodeint_at_index - inserts a new node in a linked list,
+ * at a given position
+ * @head: pointer to the first node in the list
+ * @idx: index where the new node is added
+ * @n: data to insert in the new node
  *
- * Return: The address of the new node, or NULL if it failed.
+ * Return: pointer to the new node, or NULL
  */
-
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new;
-	listint_t *current;
 	unsigned int i;
-
-	if (head == NULL)
-	{
-	return (NULL);
-	}
+	listint_t *new;
+	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-	{
+	if (!new || !head)
 		return (NULL);
-	}
+
 	new->n = n;
+	new->next = NULL;
+
 	if (idx == 0)
 	{
 		new->next = *head;
 		*head = new;
 		return (new);
 	}
-	current = *head;
-	for (i = 0; i < idx - 1 && current != NULL; i++)
+
+	for (i = 0; temp && i < idx; i++)
 	{
-		current = current->next;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
-	if (i != idx - 1 || current == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-		new->next = current->next;
-		current->next = new;
-		return (new);
+
+	return (NULL);
 }
